a0269f6c1d977241c33be90bb11277439d0e470a
mao0000 <maopuregummy393@gmail.com>
2025-12-11 16:49:12 +0900

    プロトタイプ編集画面の制限（訂正）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -20,27 +21,25 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(params[:id])
+      redirect_to prototype_path(@prototype)
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
     prototype.destroy
+    @prototype.destroy
     redirect_to root_path
   end
 
@@ -48,6 +47,10 @@ class PrototypesController < ApplicationController
   
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def move_to_index
