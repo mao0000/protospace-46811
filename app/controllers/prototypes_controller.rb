d03e25e806fd9ce17cb1a73bf20a3f3f0f7c1a7b
mao0000 <maopuregummy393@gmail.com>
2025-12-11 15:27:03 +0900

    プロトタイプ編集画面の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, only: [:edit]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    unless @prototype.user == current_user
       redirect_to root_path
     end
   end
